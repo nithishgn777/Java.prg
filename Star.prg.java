b66f597ffd798d9f8f8f611eb2c17e253b1b178c
nithishgn777 <nithishgn84@gmail.com>
2025-09-03 21:17:14 +0530

    Create Star.prg.java

---
+++ Star.prg.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n=input.nextInt();
+        for(int row=1;row<=n;row++) {
+            for (int col = 1; col <= row; col++) {
+                System.out.print("*");
+            }
+            for (int col = 1; col <= 2 * (n - row); col++) {
+                System.out.print(" ");
+
+            }
+            for (int col = 1; col <= row; col++) {
+                System.out.print("*");
+            }    System.out.println();
+
+        }
+        for(int row=n-1;row>=1;row--) {
+            for (int col = 1; col <= row; col++) {
+                System.out.print("*");
+            }
+            for (int col = 1; col <= 2 * (n - row); col++) {
+                System.out.print(" ");
+
+            }
+            for (int col = 1; col <= row; col++) {
+                System.out.print("*");
+            }    System.out.println();
+
+        }
+
+
+
+    }
+}
